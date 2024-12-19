74c9171b666da541c601c7fd9b219ac8b09b2d2a
Ilya Bylich <ibylich@gmail.com>
2024-12-19 09:29:58 +0100

    day 19 part 1

---
+++ src/bin/day19/part1.rs
@@ -0,0 +1,49 @@
+fn main() {
+    let input = include_str!("input.txt");
+    let output = solve(input);
+    println!("{}", output);
+}
+
+fn solve(input: &str) -> usize {
+    let (patterns, lines) = input.trim().split_once("\n\n").unwrap();
+    let patterns = patterns.split(", ").collect::<Vec<_>>();
+
+    let lines = lines.lines().collect::<Vec<_>>();
+
+    let mut out = 0;
+    for line in lines {
+        let mut dp = vec![0_usize; 1000];
+
+        for offset in (0..=line.len() - 1).rev() {
+            let mut total = 0;
+            let trailing = &line[offset..line.len()];
+            for pattern in patterns.iter() {
+                if trailing.starts_with(pattern) {
+                    let rest_starts_at = offset + pattern.len();
+                    let rest = &line[rest_starts_at..];
+
+                    total += dp[rest_starts_at];
+
+                    if rest.is_empty() {
+                        total += 1;
+                    }
+                }
+            }
+            dp[offset] = total
+        }
+
+        if dp[0] != 0 {
+            out += 1;
+        }
+    }
+
+    out
+}
+
+#[test]
+fn test1() {
+    let input = include_str!("input_test.txt");
+    let output = solve(input);
+    assert_eq!(output, 6);
+    // for input.txt the answer is 344
+}
